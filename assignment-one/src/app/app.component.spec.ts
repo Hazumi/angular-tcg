9c7075b96a1ab4acbee2730624947152f35eda56
Hazumi <shirohazumi@openmailbox.org>
2020-01-06 22:38:25 -0500

    first assignment

--- assignment-one/src/app/app.component.spec.ts
+++ assignment-one/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tcg'`, () => {
+  it(`should have as title 'assignment-one'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tcg');
+    expect(app.title).toEqual('assignment-one');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tcg app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('assignment-one app is running!');
   });
 });
